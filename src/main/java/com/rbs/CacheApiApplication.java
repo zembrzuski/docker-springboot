909564a82563e1aa75bd32c5aba6b1787d1be716
rodrigo zembrzuski <zembrzuski@gmail.com>
2016-10-04 17:13:22 -0300

    first commit

---
+++ src/main/java/com/rbs/CacheApiApplication.java
@@ -0,0 +1,12 @@
+package com.rbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheApiApplication.class, args);
+	}
+}
---
+++ src/main/java/com/rbs/Greeting.java
@@ -0,0 +1,20 @@
+package com.rbs;
+
+public class Greeting {
+    private long l;
+    private String format;
+
+    public Greeting(long l, String format) {
+        this.l = l;
+        this.format = format;
+    }
+
+    public long getL() {
+        return l;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+}
